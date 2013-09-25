d38647e0c4dbfbe55be0eed67c372df84fc35362
Greg Martin <gremartin@yahoo.com>
2013-09-24 23:08:12 -0400

    fixed errors in variables

--- display.php
+++ display.php
@@ -2,10 +2,17 @@
 <html>
 <head>
 <$php require_once('logic.php'); ?>
-
+<style type="text/css">
+	body{
+		background-color: rgb(<?php echo $backgroundColor;?>);
+	}
+	h2{
+		color rgb(<?php echo $textColor;?>);
+	}
+</style>
 <title>dwa15 HW 2</title>
 </head>
 <body>
-<h2>The current time is <?php echo ?currentTime;?> </h2>
+<h2>The current time is <?php echo $currentTime;?> </h2>
 </body>
 </html>
--- logic.php
+++ logic.php
@@ -1,12 +1,12 @@
 <?php
-?currentHour = date(G);
-?currentTime = date(H:i);
-if (?currentHour >= 8 and <= 20){
-	?backgroundColor = "41,151,244";
-	?textColor = "black";
+$currentHour = date(G);
+$currentTime = date(H:i);
+if ($currentHour >= 8 and <= 20){
+	$backgroundColor = "41,151,244";
+	$textColor = "black";
 	}
 else {
-	?backgroundColor = "7,11,18";
-	?textColor = "white";
+	$backgroundColor = "7,11,18";
+	$textColor = "white";
 	}
 ?>
