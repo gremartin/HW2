7d47cf94483ebd7482c30c8b2a2f7947fff30662
Greg Martin <gremartin@yahoo.com>
2013-09-24 22:36:47 -0400

    added two new files

---
+++ display.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+<$php require_once('logic.php'); ?>
+
+<title>dwa15 HW 2</title>
+</head>
+<body>
+<h2>The current time is <?php echo ?currentTime;?> </h2>
+</body>
+</html>
